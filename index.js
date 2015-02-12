eb147433ec396cee6148d0a1bf263a8c2b41bd45
Duncan Fermor <duncan@elitemail.org>
2015-02-12 23:46:31 +0000

    initial hello world

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+http.listen(3000, function() {
+    console.log('listening on *:3000');
+});
