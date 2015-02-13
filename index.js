8f391786cc9e3722cac9aa0c666c1300a2ea17c1
Duncan Fermor <duncan@elitemail.org>
2015-02-13 01:53:08 +0000

    baconify socket.io

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
+function rest(action, message) {
+  return {
+    'action': action,
+    'params': message
+  };
+}
+
+function make_message(content) {
+  return { 'content' : content };
+}
+
 var express  = require('express');
 var app      = express();
 var http     = require('http').Server(app);
 
-app  = require('./routes/root').modify(app);
-app  = require('./routes/handlebars').modify(app);
+app = require('./routes/root').modify(app);
+app = require('./routes/handlebars').modify(app);
 
 app.use(express.static(__dirname + '/public'));
 
-http = require('./routes/restful-socket').add_channel('message', http);
+bacon_http = require('./routes/restful-socket').add_channel('message', http);
+http = bacon_http.http;
+broadcast_messages = bacon_http.broadcast;
+
+//var test_msg = rest('create', make_message('testing, 1, 2, 3!'))
+//setTimeout(function() { broadcast_messages.push(test_msg) }, 5000);
 
 http.listen(3000, function() {
     console.log('listening on *:3000');
 });
+
--- routes/restful-socket.js
+++ routes/restful-socket.js
@@ -1,12 +1,22 @@
 var socketio = require('socket.io')
+var Bacon    = require('baconjs')
+
 module.exports.add_channel = function(channel, http) {
   var io = socketio(http);
 
+  var broadcast = new Bacon.Bus();
+
   io.on('connection', function(socket) {
     socket.on(channel, function(msg) {
-      io.emit(channel, msg);
+      broadcast.push(msg);
     });
   });
 
-  return http;
+  broadcast.onValue(function(msg) {
+    console.log('broadcasting', msg);
+    io.emit(channel, msg);
+  });
+
+  return { 'http': http,
+           'broadcast': broadcast };
 }
