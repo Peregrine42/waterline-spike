e58dc777dde11bf0848216bdc4393d4255983333
Duncan Fermor <duncan@elitemail.org>
2015-02-17 13:17:21 +0000

    beginning to baconify

--- index.js
+++ index.js
@@ -9,9 +9,10 @@ function make_message(content) {
   return { 'content' : content };
 }
 
-var express  = require('express');
-var app      = express();
-var http     = require('http').Server(app);
+var Bacon   = require('baconjs')
+var express = require('express');
+var app     = express();
+var http    = require('http').Server(app);
 
 app = require('./routes/root').modify(app);
 app = require('./routes/handlebars').modify(app);
@@ -20,10 +21,19 @@ app.use(express.static(__dirname + '/public'));
 
 bacon_http = require('./routes/restful-socket').add_channel('message', http);
 http = bacon_http.http;
-broadcast_messages = bacon_http.broadcast;
+
+to_database = bacon_http.broadcast;
+var broadcast_messages = new Bacon.Bus();
+
+broadcast_messages.plug(to_database);
+var changes_to_database = to_database.map(function(message) {
+  message.test = 'hi';
+  return message;
+});
 
 // TODO: debug output
-broadcast_messages.onValue(function(msg) {console.log(msg)});
+broadcast_messages.onValue(function(msg) {console.log("broadcasting:", msg)});
+//mapped_to_database.onValue(function(msg) {console.log("to database:", msg)});
 
 // TODO: for testing
 //var test_msg = rest('create', make_message('testing, 1, 2, 3!'))
